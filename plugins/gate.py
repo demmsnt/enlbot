5f50198721d7fb0e907a6537c73bd460df0b56ef
Dmitry Shamov <demmsnt@gmail.com>
2015-03-27 16:21:15 +0300

    Add photo

--- plugins/gate.py
+++ plugins/gate.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import unidecode
+import hangups
 
 def on_hg_message(coord, ev):
     CONF_ID = coord.config['hgrooms'][0]
@@ -9,11 +11,23 @@ def on_hg_message(coord, ev):
        coord.channels['XMPP'].send_message(mto=coord.config['room'],
                               mbody='({}): {}'.format(user.full_name,ev.text),
                               mtype='groupchat')    
-    if ev.conversation_id in CONF_ID and ev.text.startswith('%help'):
+    text = unidecode.unidecode(ev.text)
+    if ev.conversation_id in CONF_ID and text.startswith('%h'):
        conversation = coord.channels['HG']._conv_list.get(coord.config['hgrooms'][0])
-       text = 'j:(\nHello i am bot - you can fork me https://github.com/demmsnt/enlbot '
-       coord.channels['HG'].send_message(conversation , text)
-    
+       segments = []
+       segments.append(hangups.ChatMessageSegment('j:(\nHello i am bot - you can '))
+       segments.append(hangups.ChatMessageSegment('fork me', link_target='https://github.com/demmsnt/enlbot'))
+       segments.append(hangups.ChatMessageSegment('  '))
+       segments.append(hangups.ChatMessageSegment('\n', hangups.SegmentType.LINE_BREAK))
+       segments.append(hangups.ChatMessageSegment('Инфа об игре', link_target='http://networkglobal.ru:8090/%D0%9A%D0%B0%D0%BA%D0%98%D0%B3%D1%80%D0%B0%D1%82%D1%8C'))
+       coord.channels['HG'].send_message_segments(conversation , segments)
+
+    if ev.conversation_id in CONF_ID and text.startswith('InkReD'):
+       conversation = coord.channels['HG']._conv_list.get(coord.config['hgrooms'][0])
+       segments = []
+       segments.append(hangups.ChatMessageSegment(' InkReD', link_target='https://lh3.googleusercontent.com/-V2xwgnVmS1k/VRVYUpXKAsI/AAAAAAAAABg/W0MgJyOfxak/s0/InkRed1.png'))
+       coord.channels['HG'].send_message_segments(conversation , segments)
+       
            
 def on_xmpp_start(coord):
     room = coord.config['room']
