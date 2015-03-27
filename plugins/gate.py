20247073b51975e44c397de422ce511f6cc78af0
Dmitry Shamov <demmsnt@gmail.com>
2015-03-27 15:01:42 +0300

    Add help

--- plugins/gate.py
+++ plugins/gate.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 def on_hg_message(coord, ev):
     CONF_ID = coord.config['hgrooms'][0]
     conv = coord.channels['HG']._conv_list.get(ev.conversation_id)
@@ -10,6 +9,11 @@ def on_hg_message(coord, ev):
        coord.channels['XMPP'].send_message(mto=coord.config['room'],
                               mbody='({}): {}'.format(user.full_name,ev.text),
                               mtype='groupchat')    
+    if ev.conversation_id in CONF_ID and ev.text.startswith('%help'):
+       conversation = coord.channels['HG']._conv_list.get(coord.config['hgrooms'][0])
+       text = 'j:(\nHello i am bot - you can fork me https://github.com/demmsnt/enlbot '
+       coord.channels['HG'].send_message(conversation , text)
+    
            
 def on_xmpp_start(coord):
     room = coord.config['room']
@@ -24,4 +28,11 @@ def on_xmpp_message(coord, msg):
             if text.startswith('#'):
                conversation = coord.channels['HG']._conv_list.get(coord.config['hgrooms'][0])
                coord.channels['HG'].send_message(conversation , 'j:({}): {}'.format(str(msg['mucnick']), text[1:]))
+            if text.startswith('%hglist'):
+               text = []
+               for c in coord.channels['HG']._conv_list.get_all():
+                   text.append('({}) {}'.format(c.id_, c.name))
+               coord.channels['XMPP'].send_message(mto=coord.config['room'],
+                              mbody='\n{}'.format('\n'.join(text)),
+                              mtype='groupchat')    
 
